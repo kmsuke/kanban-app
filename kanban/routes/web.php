d0951051ce1d92acef4e98cb5971240b44e617b9
kmsuke <kmuradsuke343@gmail.com>
2019-06-09 22:47:32 +0900

    coding complete

--- kanban/routes/web.php
+++ kanban/routes/web.php
@@ -15,6 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//リスト一覧画面
+Route::get('/','ListingsController@index');
+
+//リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
+
+//リスト新規処理
+Route::post('/listings','ListingsController@store');
+
+//リスト更新画面
+Route::get('/listingsedit/{listing_id}', 'ListingsController@edit');
+
+//リスト更新処理
+Route::post('/listing/edit','ListingsController@update');
+
+//リスト削除処理
+Route::get('/listingsdelete/{listing_id}', 'ListingsController@destroy');
+
+//カード新規画面
+Route::get('listing/{listing_id}/card/new', 'CardsController@new')->name('new_card');
+
+//カード新規処理
+Route::post('/listing/{listing_id}/card', 'CardsController@store');
+
+//カード詳細画面
+Route::get('listing/{listing_id}/card/{card_id}', 'CardsController@show');
+
+//カード編集画面
+Route::get('listing/{listing_id}/card/{card_id}/edit', 'CardsController@edit');
+
+//カード更新処理
+Route::post('/card/edit', 'CardsController@update');
+
+//カード削除処理
+Route::get('listing/{listing_id}/card/{card_id}/delete', 'CardsController@destroy');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
